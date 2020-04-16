51e0cdef9dfc6ba92e734ef95f48f2e734a40ba8
Antti J Manninen <antti.manninen@fmi.fi>
2020-04-16 13:36:29 +0300

    latest

--- scripts/DIAL_xco2_retrieval.py
+++ scripts/DIAL_xco2_retrieval.py
@@ -8,27 +8,36 @@ import numpy as np
 from dialpy.pyOptimalEstimation import pyOptimalEstimation as pyOE
 from dialpy.equations.differential_co2_concentration import xco2_power
 
+# Name of data fields
 priori_names = ["co2_ppm"]
 obs_names = ["range_", "delta_sigma_abs", "power_on", "power_off", "power_bkg"]
 
+# Prepare priori and its uncertainty
 co2_ppm_model = -np.linspace(0, 1, 320)**2 * np.linspace(0, 1, 320) + np.repeat(400, 320)
 co2_ppm_model_sigma = np.repeat(3, 320) + 1 * np.random.rand(320)
 
-
-power_off = -np.linspace(0,1,320)**2 * np.linspace(0,1,320) + 9.9
-power_on_hi = -np.linspace(0,1,320)**2 * np.linspace(0,1,320) + 9.8
+# Prepare observed variables
+range_ = np.arange(0, 9600, 30)
+power_off = -np.linspace(0, 1, 320)**2 * np.linspace(0, 1, 320) + 9.9
+power_on_hi = -np.linspace(0, 1, 320)**2 * np.linspace(0, 1, 320) + 9.8
 power_on = np.hstack((power_off[:100], power_on_hi[100:]))
-power_bkg = np.linspace(0,1,320) + (np.random.rand(320)+8)
+power_bkg = np.linspace(0, 1, 320) + (np.random.rand(320)+8)
+delta_sigma_abs = 1
 
+# Arrange forward model inputs to dictionary
 xco2_args = {"range_": range_,
              "delta_sigma_abs": delta_sigma_abs,
              "power_on": power_on,
              "power_off": power_off,
              "power_bkg": power_bkg}
 
-obs = [range_, delta_sigma_abs, power_on, power_off, power_background]
+# List observations
+obs = [range_, delta_sigma_abs, power_on, power_off, power_bkg]
+
+# Covariance matrix of observations
 obs_sigma = np.cov([range_, delta_sigma_abs, power_on, power_off, power_bkg])
 
+# Create optimal estimation object
 oe = pyOE.optimalEstimation(
     priori_names,  # state variable names
     co2_ppm_model,  # a priori
@@ -37,8 +46,10 @@ oe = pyOE.optimalEstimation(
     obs,  # observations
     obs_sigma,  # observation uncertainty
     xco2_power,  # forward Operator
-    forwardKwArgs=xco2_args,  # additonal function arguments
-)
+    forwardKwArgs=xco2_args)  # additional function arguments
+
+# Do retrieval
+converged = oe.doRetrieval(maxIter=10)
 
 
 
